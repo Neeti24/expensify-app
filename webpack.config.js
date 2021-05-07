cb12ea55abf3b496bf8c0204f5d48fbaef7486fc
Neeti <dawarneeti24@gmail.com>
2021-05-07 20:46:54 +0530

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,56 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback : true
-  }
+module.exports = (env, argv) => {
+// const isProduction = env === 'production';
+const CSSExtract = new MiniCssExtractPlugin({ filename : 'styles.css' });
+  return {
+    mode : env.production ? 'production' : 'develpoment',
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: [
+          {
+            loader : MiniCssExtractPlugin.loader
+           },
+           {
+             loader : 'css-loader',
+             options : {
+               sourceMap : true
+             }
+           },
+           {
+             loader : 'sass-loader',
+             options : {
+               sourceMap : true
+             }
+           }
+        ]
+      }]
+    },
+    plugins : [
+      CSSExtract,
+      new TerserPlugin({
+        terserOptions: {
+         compress: argv.mode === 'production' // only if `--mode production` was passed
+        }
+      })
+    ],
+    devtool: env.production ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback : true
+    }
+  };
 };
+
